e8ff976117e7e899d478ae11e50fafbc141acaf3
Marcus Moreno <legendary.1198@gmail.com>
2020-07-18 20:36:24 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,29 +1,29 @@
 class String
 
   def sentence?
-    
+
     self.end_with?(".")
     
   end
   
 
   def question?
-    
+
     self.end_with?("?")
     
   end
   
 
   def exclamation?
-    
+
     self.end_with?("!")
     
   end
   
 
   def count_sentences
-    
-    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+
+    self.split(/[.?!]+/).count
     
   end
-end
+end 
